5f12d723427f70d02e366aef440aadcec6c253b2
David Brown <davidbrown@davids-MacBook-Pro.local>
2022-04-05 12:39:04 -0500

    completed assinment 3 part 1

--- assignment/scripts/array-practice.js
+++ assignment/scripts/array-practice.js
@@ -8,11 +8,11 @@ let animalArray = ['fish', 'cat', 'bird', 'dog'];
 console.log('Animals are: ', animalArray);
 
 // 1.a. TODO: Make an array with some favorite foods
-
+let favoriteFoods = ["pizza","potato chips","pasta"];
 
 // 1.b. TODO: Log your array of foods to the console with a message, similar 
 //      to the example above
-
+console.log("These are my favorite foods:", favoriteFoods);
 
 
 // 2. Array.length - tells you how many items are in the array
@@ -22,23 +22,23 @@ console.log('--- 2. Length of an array ---');
 console.log('Number of animals:', animalArray.length);
 
 // 2.a. TODO: Log to the console the number of foods in your array
-
+console.log("Number of Foods:" ,favoriteFoods.length);
 
 // 3. Accessing array items
 console.log('--- 3. Accessing items in an array ---');
 
 // Example: Log the first animal from the array using it's array index
-console.log('First animal is', animalArray[0])
+console.log('First animal is', animalArray[0]);
 
 // 3.a. TODO: Log the second animal in the array 
-
+console.log("The second animal is",animalArray[1]);
 
 // 3.b. TODO: Log the last animal in the array using it's array index 
-
+console.log(animalArray[3]);
 
 // 3.c. (STRETCH) TODO: Log the last animal by using the array length, 
 //      instead of the exact index number of the last item
-
+console.log(animalArray[animalArray.length -1]);
 
 
 // 4. Adding & Removing Array Items
@@ -49,7 +49,8 @@ animalArray.push('penguin');
 console.log('Added an animal to end,', animalArray);
 
 // 4.a. TODO: Add a new food at the end of your array & log the array
-
+favoriteFoods.push("crab");
+console.log("Added a food" ,favoriteFoods);
 
 
 // Example: Remove the last animal by using Array.pop
@@ -59,14 +60,16 @@ console.log('The animals are now', animalArray);
 
 // 4.b. TODO: Remove the food at the end of your array & 
 //      log both the food removed and the updated array
-
-
+let removedFood = favoriteFoods.pop();
+console.log("This was removed" ,removedFood)
+console.log("the array is now", favoriteFoods)
 // Example: Add an animal to the beginning using Array.unshift
 animalArray.unshift('walrus');
 console.log(`Added an animal to beginning: ${animalArray}`);
 
 // 4.c. TODO: Add a food at the beginning of the array & log the array
-
+favoriteFoods.unshift("shrimp");
+console.log(favoriteFoods);
 
 // Example: Remove the first animal using Array.shift
 removedAnimal = animalArray.shift();
@@ -75,20 +78,32 @@ console.log('The animals are now', animalArray);
 
 // 4.d TODO: Remove the food at the beginning of your array & 
 //     log both the food removed and the updated array
+removedFood = favoriteFoods.shift();
+console.log("This food was removed" ,removedFood);
+console.log("current list of favorite foods",favoriteFoods);
 
 // 4.e (STRETCH) TODO: Replace the second food in your array
 //      with another one of your favorite foods.
 //      Then log the updated array.
-
+favoriteFoods[1]=("fried shrimp");
+console.log(favoriteFoods);
 // 4.f (STRETCH) TODO: Sort your favoriteFoods array
 //     in reverse alphabetical order. Log the array.
-
+favoriteFoods.sort(function(a,b){
+    return a - b
+});
+console.log(favoriteFoods);
 // 4.g (STRETCH) TODO: Convert your array to a string
 //     putting the word "and" between each item.
 //     eg "tacos and pizza and pasta". Log the string.
-
+favoriteFoodsList = favoriteFoods.toString();
+let string = favoriteFoodsList.replaceAll("," ," " + "and" + " " );
+console.log(string);
 // 4.h (STRETCH) TODO: Make a new array that combines 
 //     the favorite foods array with the animals array.
 //     Then log the new array.
 //     It should look something like:
 //     ['pizza', 'pasta', 'fish', 'cat', 'bird', 'dog']
+let animalArrayClone = animalArray.slice();
+favoriteFoods = favoriteFoods.concat(animalArrayClone);
+console.log(favoriteFoods);
